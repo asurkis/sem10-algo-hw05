e00328f6bb2f67d9865b829bff9c30cb2dccd211
Anton Surkis <antonsurkis@gmail.com>
2023-03-19 18:34:25 +0300

    Solve task 8

--- hs/src/Lib.hs
+++ hs/src/Lib.hs
@@ -1,10 +1,4 @@
 module Lib where
 
-bin :: [Integer]
-bin=fst<$>iterate(\(x,p)->(x*p+p-1-x,p*p))(0,2)
-
-dec :: [Integer]
-dec=fst<$>iterate(\(x,p)->(x*p+div(p-1)9-x,p*p))(0,10)
-
-str :: [String]
-str=(\(x,p)->tail$show$p+x)<$>iterate(\(x,p)->(x*p+div(p-1)9-x,p*p))(0::Integer,10)
+ans :: [Int]
+ans=0:concat(iterate(\l->l++map(1-)l)[1])
